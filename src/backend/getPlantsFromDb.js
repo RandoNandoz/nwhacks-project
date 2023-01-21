5b32a8a55e5031781e42a87462398de1e1ae551d
Randy Zhu <mail@randyzhu.com>
2023-01-21 14:57:21 -0800

    barely working writeplant, read plant no work

---
+++ src/backend/getPlantsFromDb.js
@@ -0,0 +1,41 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase, child, get, ref } from "firebase/database";
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDhQtFyzVzn8HyImunbxYc_dyzArZg0010",
+
+    authDomain: "plantis-8ff44.firebaseapp.com",
+
+    databaseURL: "https://plantis-8ff44-default-rtdb.firebaseio.com",
+
+    projectId: "plantis-8ff44",
+
+    storageBucket: "plantis-8ff44.appspot.com",
+
+    messagingSenderId: "157187864759",
+
+    appId: "1:157187864759:web:dc4a544c9f1082b0937532"
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getDatabase(app);
+
+const getPlantsFromDb = () => {
+    // read all plant data from db
+    const dbRef = ref(db, 'plants');
+    get(child(dbRef, 'plants')).then((snapshot) => {
+        if (snapshot.exists()) {
+            console.log(snapshot.val());
+        } else {
+            console.log("No data available");
+        }
+    }).catch((error) => {
+        console.error(error);
+    }
+    );
+}
+
+export default getPlantsFromDb
---
+++ src/backend/writePlantToDb.js
@@ -0,0 +1,43 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set } from "firebase/database";
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDhQtFyzVzn8HyImunbxYc_dyzArZg0010",
+
+    authDomain: "plantis-8ff44.firebaseapp.com",
+
+    databaseURL: "https://plantis-8ff44-default-rtdb.firebaseio.com",
+
+    projectId: "plantis-8ff44",
+
+    storageBucket: "plantis-8ff44.appspot.com",
+
+    messagingSenderId: "157187864759",
+
+    appId: "1:157187864759:web:dc4a544c9f1082b0937532"
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getDatabase(app);
+
+// const readPlantsFromDb() => {
+//     const dbRef = db;
+// }
+
+const writePlantToDb = (name, desc, image_url, water_every, pruning, soil, sunlight, fertilzer) => {
+    set(ref(db, 'plants/' + name), {
+        "name": name,
+        "desc": desc,
+        "image_url": image_url,
+        "water_every": water_every,
+        "pruning": pruning,
+        "soil": soil,
+        "sunlight": sunlight,
+        "fertilzer": fertilzer
+    })
+}
+
+export default writePlantToDb
