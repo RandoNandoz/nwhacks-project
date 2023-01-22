589686a4527e070a27d7f6f03cb52f06dcd9c833
mikuro39 <121419505+mikuro39@users.noreply.github.com>
2023-01-21 21:51:32 -0800

    added more plants

--- src/pages/info.js
+++ src/pages/info.js
@@ -7,7 +7,7 @@ export default function Info() {
 					Plant Info
 				</h1>
                 <div class="plants">
-                    <h3 class="plant-name">Succulent</h3>
+                    <h3 class="plant-name" id="plant1">Succulent</h3>
                     <div class="non-title">
                         <img class="plant-pic" src=
                             "https://www.ikea.com/ca/en/images/products/fejka-artificial-potted-plant-with-pot-indoor-outdoor-succulent__0614211_pe686835_s5.jpg?f=s"
@@ -26,10 +26,10 @@ export default function Info() {
                         </p>
                         </div>
 
-                        <h3 class="plant-name">Plant 2</h3>
+                        <h3 class="plant-name" id="plant2">Chinese Money Plant</h3>
                     <div class="non-title">
                         <img class="plant-pic" src=
-                            "https://www.ikea.com/ca/en/images/products/fejka-artificial-potted-plant-indoor-outdoor-grass__0130933_pe285358_s5.jpg"
+                            "https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_1:1/at%2Farchive%2F5e10897d140daa69e35e3d6600699432f0e39ceb"
                             alt=""/>
                         <p class="description">
                         The Chinese Money Plant, also known as Pilea peperomioides, is a popular indoor grass plant. 
@@ -37,6 +37,51 @@ export default function Info() {
                         thin stems that can reach up to 12 inches tall. It is a low maintenance plant that thrives in bright, 
                         indirect light and well-draining soil. It is also known to be a good air purifying plant. The Chinese 
                         Money Plant is easy to propagate and makes a great addition to any indoor garden.
+                        </p>
+                    </div>
+                    <h3 class="plant-name" id="plant3">Spider Plant</h3>
+                    <div class="non-title">
+                        <img class="plant-pic" src=
+                            "https://cdn.shopify.com/s/files/1/0210/9472/products/IMG_9675_649fa94f-e955-4393-a2f6-c65d2081aa95.jpg?v=1653514453"
+                            alt=""/>
+                        <p class="description">
+                        The Spider Plant, also known as the Chlorophytum comosum, is a popular houseplant known for its easy care 
+                        and ability to purify the air. It has long, thin green leaves that resemble spider legs, giving it its name. 
+                        The plant produces small white flowers on long stalks and can also produce small plantlets, or "spiderettes," 
+                        on the ends of its leaves. It prefers indirect sunlight and well-drained soil, and can tolerate a wide range 
+                        of temperatures. It is a great plant for beginners or for those who want to improve the air quality in their home.
+                        </p>
+                    </div>
+                    <h3 class="plant-name" id="plant4">Aloe Vera</h3>
+                    <div class="non-title">
+                        <img class="plant-pic" src=
+                            "https://www.ikea.com/ca/en/images/products/aloe-vera-potted-plant-aloe__67410_pe181254_s4.jpg"
+                            alt=""/>
+                        <p class="description">
+                        The Aloe vera plant is a succulent species that belongs to the family Asphodelaceae. 
+                        It is a perennial plant that can grow up to 2 feet tall and 2 feet wide. The leaves are thick and fleshy, 
+                        with a deep green color and a serrated edge. The leaves are also covered in a thin layer of white, powdery 
+                        substance, which is called aloin. The plant produces yellow or orange flowers that bloom in the summer. 
+                        The sap of the Aloe vera plant is well known for its medicinal properties and is often used to treat burns, 
+                        cuts, and skin irritations. It is a low maintenance plant that is easy to care for and can thrive in a variety 
+                        of conditions, including low light and dry soil. It is a great addition to any indoor or outdoor garden, and is 
+                        a popular choice for use in landscaping and as a houseplant.
+                        </p>
+                    </div>
+                    <h3 class="plant-name" id="plant5">Snake Plant</h3>
+                    <div class="non-title">
+                        <img class="plant-pic" src=
+                            "https://www.ikea.com/ca/en/images/products/sansevieria-trifasciata-potted-plant-mother-in-laws-tongue__0908898_pe676659_s5.jpg"
+                            alt=""/>
+                        <p class="description">
+                        The snake plant, also known as Sansevieria trifasciata, is a hardy and versatile succulent that is well-suited 
+                        for both indoor and outdoor environments. With its tall, stiff leaves that resemble snakeskin, this plant is a 
+                        striking addition to any space. The leaves are a dark green color, with vertical yellowish-green stripes running 
+                        along their length. They can grow up to 3-4 feet tall, and the leaves can reach up to a foot in width. The snake 
+                        plant is known for its low maintenance and ability to thrive in low light conditions. It's also known for its 
+                        air-purifying capabilities, making it a great choice for indoor spaces. It's easy to propagate, making it perfect 
+                        for those who want to grow their own snake plant collection. It's also a great option for those who want to add a 
+                        bit of green to their home without having to spend a lot of time on maintenance.
                         </p>
                     </div>
 				</div>
