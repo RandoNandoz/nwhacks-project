26a0cc837bcc1cc07ac241e811cdcae476a044dc
Evlav <varotpava@gmail.com>
2023-01-21 17:50:22 -0800

    Home Page
    Changed colors + Logo on Header + Home page furnishing

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,6 +1,10 @@
 export default function Navbar () {
     return <nav className="nav">
+        <div className="container1">
         <a href="/" className="site-title">Plantis</a>
+        <img src="https://cdn.discordapp.com/attachments/1066426699669057737/1066526497747243058/IMG_0391.png" height={100}/>
+        </div>
+        
         <ul>
             <li>
                 <a href="/pages/info">Info</a>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,3 +1,32 @@
+import Stack from 'react-bootstrap/Stack';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import './home.css'
+
+
 export default function Home() {
-    return <h1>Home</h1>
+    return (
+       
+        <Container>
+        <Row>
+        <Col>
+          <div className="bg-light border">Plantis</div>
+          <div className="bg-light border">For all your plant care needs</div>  
+          <ButtonGroup aria-label="Basic example">
+                <Button variant="secondary" href='/pages/login'>Sign Up</Button>
+                <Button variant="secondary" href='/pages/login'>Log In</Button>
+            </ButtonGroup>
+        </Col>
+
+          <Col><img src='https://media.istockphoto.com/id/1045368942/fr/vectoriel/cr%C3%A9ation-de-feuille-verte-abstraite-logo-ic%C3%B4ne-vector-jeu-dic%C3%B4nes-de-l%C3%A9cologie-ic%C3%B4ne-eco.jpg?s=612x612&w=0&k=20&c=mSeHhuilvfCOyvj-tIwYFFqjCNkJZJWGQV_atSMBTyQ='/>
+        </Col>
+        </Row>
+      </Container>
+     
+    
+    )
 }
