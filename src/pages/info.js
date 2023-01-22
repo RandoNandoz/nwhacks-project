7fab74c67644da57067bcaa5fa077bf7d5a5019b
michelle <mzhou25@students.cs.ubc.ca>
2023-01-21 17:36:02 -0800

    Added log page and timetable

--- src/pages/info.js
+++ src/pages/info.js
@@ -1,3 +1,36 @@
-export default function info() {
-    return <h1>Info</h1>
+import "./info.css";
+// long info page
+export default function Info() {
+    return (
+		<div class="everything">
+			<section class="section">
+				<div class="box-main">
+					<div class="firstHalf">
+						<h1 class="text-big">
+							Plant Info
+						</h1>
+                        <div class="description">
+                            <h3>Succulent</h3>
+                            <img class="plant-pic" src=
+                            "https://www.ikea.com/ca/en/images/products/fejka-artificial-potted-plant-with-pot-indoor-outdoor-succulent__0614211_pe686835_s5.jpg?f=s"
+							alt=""/>
+                            <p class="text-small">
+                                The Desert Rose is a unique and hardy succulent, 
+                                characterized by its thick, fleshy leaves and striking, 
+                                trumpet-shaped flowers. The leaves are a deep green color, 
+                                and are arranged in a rosette pattern, creating a beautiful, 
+                                symmetrical appearance. The Desert Rose is a slow grower, but can 
+                                reach heights of up to two feet when mature. The flowers, which bloom 
+                                in shades of pink and red, are highly fragrant and attract bees and 
+                                butterflies. This succulent is a great choice for gardeners looking 
+                                for a low-maintenance, drought-tolerant plant that adds a touch of 
+                                beauty to any landscape.
+                            </p>
+                        </div>
+					</div>
+				</div>
+			</section>
+		</div>
+    );
 }
+
