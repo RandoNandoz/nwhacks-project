66b2a000b49b4a30cbf6f294d25274e658f6975b
mikuro39 <121419505+mikuro39@users.noreply.github.com>
2023-01-22 10:36:47 -0800

    added semi functional delete and add button

--- src/pages/logpage.js
+++ src/pages/logpage.js
@@ -3,6 +3,7 @@ import Card from 'react-bootstrap/Card';
 import "./logpage.css";
 import TestPlant from '../components/test';
 import React from "react";
+import ReactDOM from 'react-dom/client';
 
 // class DeleteTest extends React.Component {
 //     constructor(props) {
@@ -43,44 +44,42 @@ class DeleteTest extends React.Component {
       return (
         <div>
         {myheader}
-        <button type="button" onClick={this.delHeader}>Delete Header</button>
+        <button type="button" onClick={this.delHeader}>Delete Plant</button>
         </div>
       );
     }
   }
+
+  class AddTest extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {show: false};
+    }
+    addHeader = () => {
+      this.setState({show: true});
+    }
+    render() {
+      let myheader;
+      if (this.state.show) {
+        myheader = <DeleteTest />;
+      };
+      return (
+        <div>
+        {myheader}
+        <button type="button" onClick={this.addHeader}>Add Plant</button>
+        </div>
+      );
+    }
+  }
+
 
 export default function LogPage() {
     return (
         <div className='container1 log'>
             <div class="cards">
                 <h1 class="title">Plant Log</h1>
-                <Button variant="primary" size="lg" id='one1'>
-                    New Plant
-                </Button>{' '}
-
-                <Button variant="primary" size="lg" id='one1'>
-                    Edit Plant
-                </Button>{' '}
-
-                <Button variant="primary" size="lg" id='one1'>
-                    Delete Plant
-                </Button>{' '}
-
-                <div className='CardContainer'>
-                    <Card style={{ width: '18rem' }} bg={"Success"}>
-                        <Card.Img variant="top" src="https://www.ikea.com/ca/en/images/products/fejka-artificial-potted-plant-with-pot-indoor-outdoor-succulent__0614211_pe686835_s5.jpg?f=s" />
-                        <Card.Body>
-                        <Card.Title>
-                          Succulent
-                        </Card.Title>
-                        <Card.Subtitle>
-                          Name:
-                        </Card.Subtitle>
-                        <Button variant="primary" href='/pages/info#plant1'>More info</Button>
-                        </Card.Body>
-                    </Card>
+                    <AddTest />
                     <DeleteTest />
-                    </div>
                 </div>
               </div>
       
