65a9c4644ef19591fecf0646d5797a68973462d6
Evlav <varotpava@gmail.com>
2023-01-22 10:05:07 -0800

    Revert "Revert "added semi functional delete button""
    This reverts commit ecbdc41fbebc484a3b12dc2336b83406f6134b01.

--- src/pages/logpage.js
+++ src/pages/logpage.js
@@ -2,7 +2,35 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import "./logpage.css";
 import TestPlant from '../components/test';
+<<<<<<< Updated upstream
 import { God } from '../components/test';
+=======
+//import "./DeleteTest.js";
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
+class DeleteTest extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {show: true};
+    }
+    delHeader = () => {
+      this.setState({show: false});
+    }
+    render() {
+      let myheader;
+      if (this.state.show) {
+        myheader = <TestPlant />;
+      };
+      return (
+        <div>
+        {myheader}
+        <button type="button" onClick={this.delHeader}>Delete Header</button>
+        </div>
+      );
+    }
+  }
+>>>>>>> Stashed changes
 
 export default function LogPage() {
     return (
@@ -22,48 +50,22 @@ export default function LogPage() {
                 </Button>{' '}
 
                 <div className='CardContainer'>
-                    <TestPlant/>
-
                     <Card style={{ width: '18rem' }} bg={"Success"}>
                         <Card.Img variant="top" src="https://www.ikea.com/ca/en/images/products/fejka-artificial-potted-plant-with-pot-indoor-outdoor-succulent__0614211_pe686835_s5.jpg?f=s" />
                         <Card.Body>
-                            <Card.Title>
-                                Succulent
-                            </Card.Title>
-                            <Card.Subtitle>
-                                Name:
-                            </Card.Subtitle>
-                            <Button variant="primary" href='/pages/info#plant1'>More info</Button>
-                            </Card.Body>
+                        <Card.Title>
+                          Succulent
+                        </Card.Title>
+                        <Card.Subtitle>
+                          Name:
+                        </Card.Subtitle>
+                        <Button variant="primary" href='/pages/info#plant1'>More info</Button>
+                        </Card.Body>
                     </Card>
-                    <Card style={{ width: '18rem' }} bg={"Success"}>
-                        <Card.Img variant="top" src="https://www.ikea.com/ca/en/images/products/fejka-artificial-potted-plant-with-pot-indoor-outdoor-succulent__0614211_pe686835_s5.jpg?f=s" />
-                        <Card.Body>
-                            <Card.Title>
-                                Succulent
-                            </Card.Title>
-                            <Card.Subtitle>
-                                Name:
-                            </Card.Subtitle>
-                            <Button variant="primary" href='/pages/info#plant1'>More info</Button>
-                            </Card.Body>
-                    </Card>
-                    <Card style={{ width: '18rem' }} bg={"Success"}>
-                        <Card.Img variant="top" src="https://www.ikea.com/ca/en/images/products/fejka-artificial-potted-plant-with-pot-indoor-outdoor-succulent__0614211_pe686835_s5.jpg?f=s" />
-                        <Card.Body>
-                            <Card.Title>
-                                Succulent
-                            </Card.Title>
-                            <Card.Subtitle>
-                                Name:
-                            </Card.Subtitle>
-                            <Button variant="primary" href='/pages/info#plant1'>More info</Button>
-                            </Card.Body>
-                    </Card>
-                </div>
-                        
+                    <DeleteTest />
                     </div>
                 </div>
+              </div>
       
       );
 }
